7fd2b9a97066f4e7696f8952c43f2b2fb52acf7a
run532 <532499602@qq.com>
2014-08-24 12:59:26 +0800

    save init.inc.php

--- init.inc.php
+++ init.inc.php
@@ -4,3 +4,29 @@ $templates = array(
     '1'=>'default',
     '2'=>'format-audio'
 );
+
+function success($msg,$url=null){
+    $url=$url?"location.href='{$url}'":"window.history.go(-1)";
+    $html=<<<HTML
+	    <div style="background:#dff0d8;color:#468847;border:1px solid #d6e9c6;padding:20px;"><h2>:) $msg</h2></div>
+        <script>
+            setTimeout(function(){
+                {$url}
+            },2000)
+        </script>
+HTML;
+    echo $html;
+}
+
+function error($msg,$url=null){
+    $url=is_null($url)?"window.history.go(-1)":"location.href='{$url}'";
+    $html=<<<HTML
+    <div style="background:#f2dede;color:#b94a48;border:1px solid #eed3d7;padding:20px;"><h2>:( $msg</h2></div>
+        <script>
+            setTimeout(function(){
+                {$url}
+            },2000)
+    </script>
+HTML;
+    die($html);
+}
